ed3965d1cbc207d1c32813373ce8f68536f78ac6
HUYNHBINH\huynh <huynhbinh0511@gmail.com>
2018-03-06 11:57:37 +0700

    Commit - First draft Lottery contract

---
+++ Contracts/LotteryContract.sol
@@ -0,0 +1,109 @@
+contract Lottery {
+
+	enum State {
+        AcceptBuyTicket,
+        EndBuyTicket,
+        KillContract
+    }
+
+	 address public _owner = msg.sender;
+	 address public _co_owner1;
+	 address public _co_owner2;
+	 address public _co_owner3;
+	 
+	 uint public creationTime = now;
+ 
+ 
+ 
+    // Modifier
+    modifier onlyCLevel(){
+       require(msg.sender == 0xca35b7d915458ef540ade6068dfe2f44e8fa733c);
+        _;
+    }
+	
+	modifier onlyOwner(){
+		require(msg.sender == _owner);
+		_;
+	}
+	
+	modifier onlyAfter(uint _time) {
+        require(now >= _time);
+        _;
+    }
+	// End Modifier
+ 
+ 
+ 
+ 
+    // campaign index of lottery
+    uint CampaignID;
+    mapping (uint => Campaign) campaigns;
+    
+    struct Player{
+        address playerAddress;
+        uint16 lotteryNumber;
+    }
+    
+    struct Campaign{
+        address beneficiary;
+        uint amount;
+        uint numPlayers;
+        uint16 winningLotteryNumber;
+        uint ticketPrice;
+        bool isClosed;
+        mapping (uint => Player) players;
+    }
+	
+	
+	function changeOwner(address newOwner) public onlyOwner()
+    {
+        _owner = newOwner;
+    }
+	
+	function setCoOwner(int who, address coOwner) public onlyOwner()
+    {
+		if(who == 1)
+		{
+			_co_owner1 = coOwner;
+		}
+		else if(who == 2)
+		{
+			_co_owner2 = coOwner;
+		}
+		else if(who ==3)
+		{
+			_co_owner3 = coOwner;
+		}
+		else
+		{
+			//throw
+		}   
+    }
+    
+    
+    function createCampaign(address beneficiary ,uint ticketPrice) public onlyCLevel() returns (uint campaignID) {
+    
+        campaignID = numCampaigns++; // campaignID is return variable
+        campaigns[campaignID] = Campaign(beneficiary,0,0,0,ticketPrice,false);
+    }
+    
+    
+   function setWinningLotteryNumber(uint campainID, uint16 winningLotteryNumber) public onlyCLevel(){
+       require(campaigns[campainID].winningLotteryNumber == 0);
+       campaigns[campainID].winningLotteryNumber = winningLotteryNumber;
+       campaigns[campainID].isClosed = true;
+   }
+    
+   function getWinningLotteryNumber(uint campainID) public view returns(uint){
+       return campaigns[campainID].winningLotteryNumber;
+   } 
+   
+   function withdraw() public {
+        uint amount = pendingWithdrawals[msg.sender];
+        // Remember to zero the pending refund before
+        // sending to prevent re-entrancy attacks
+        pendingWithdrawals[msg.sender] = 0;
+        msg.sender.transfer(amount);
+    }
+    
+}
